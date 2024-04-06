a3fc9f049a1660dc77cd26c4542dc9c93bd63e6f
Samuel Luiz <samuelluizrl@outlook.com>
2024-04-06 11:32:50 -0300

    first commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx,vue}',
+    './components/**/*.{ts,tsx,vue}',
+    './app/**/*.{ts,tsx,vue}',
+    './src/**/*.{ts,tsx,vue}',
+	],
+  prefix: "",
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
